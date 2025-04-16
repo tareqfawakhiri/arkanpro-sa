7b57d011a89c9f26c2911bc2ad08882185c2d926
Tareq Fawakhiri <tareqfawahkiri@gmail.com>
2025-04-16 20:56:10 +0300

    Update BlogControlller.php

--- app/Http/Controllers/BlogControlller.php
+++ app/Http/Controllers/BlogControlller.php
@@ -8,18 +8,25 @@ use App\Models\Post;
 
 class BlogControlller extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
+     */
     function index()
     {
-          $posts = Post::with(['category', 'author'])
+        $posts = Post::with(['category', 'author'])
             ->where('status', 'published')
             ->orderBy('created_at', 'desc')
             ->paginate(12);
         return view('sections.blog', compact('posts'));
     }
 
+    /**
+     * @param $slug
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
+     */
     function details($slug)
     {
-        dd('xx xxx');
+        return $post = Post::where('slug', $slug)->firstOrFail();
         return view('sections.blog-details', compact('post'));
     }
 }

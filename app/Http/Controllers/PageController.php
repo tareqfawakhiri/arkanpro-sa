177d409748e0a123e40571bd6bcdda3c5e2e7a45
Waleed-Alhaj <116523175+Waleed-Alhaj@users.noreply.github.com>
2025-04-30 19:00:42 +0300

    pricing blade

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Service;
+use App\Models\Pricing;
 
 class PageController extends Controller
 {
@@ -13,10 +14,29 @@ class PageController extends Controller
             ->first();
 
         $title = str_replace('', '_', $section->title);
+
+        $pricing = Pricing::orderBy('created_at')->get();
+        $i = 0;
+        foreach ($pricing as $plane) {
+            $pricing[$i]->pricing_check = Pricing::select('pricing_checklist.title', 'pricing_checklist.title_ar')
+                ->leftJoin('pricing_check', 'pricing.id', '=', 'pricing_check.pricing_id')
+                ->leftJoin('pricing_checklist', 'pricing_checklist.id', '=', 'pricing_check.pricing_checklist_id')
+                ->WHERE('pricing.id', $plane->id)
+                ->get();
+
+
+            $pricing[$i]->pricing_uncheck = Pricing::select('pricing_checklist.title', 'pricing_checklist.title_ar')
+                ->leftJoin('pricing_uncheck', 'pricing.id', '=', 'pricing_uncheck.pricing_id')
+                ->leftJoin('pricing_checklist', 'pricing_checklist.id', '=', 'pricing_uncheck.pricing_checklist_id')
+                ->WHERE('pricing.id', $plane->id)
+                ->get();
+            $i++;
+        }
+
         if (view()->exists('sections.page.' . $title))
-            return view('sections.page.' . $title, compact('section'));
+            return view('sections.page.' . $title, compact('section', 'pricing'));
         else
-            return view('sections.page.index', compact('section'));
+            return view('sections.page.index', compact('section', 'pricing'));
     }
 
 }
